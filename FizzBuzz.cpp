f83c4791be6e2d60a4ed733c75c2209a7af420f7
Takanori Onuma <s1000607@sendai-nct.jp>
2014-04-23 19:15:58 +0900

    省略化
    無駄なelseを無くした
    
    Signed-off-by: Takanori Onuma <s1000607@sendai-nct.jp>

--- FizzBuzz.cpp
+++ FizzBuzz.cpp
@@ -6,25 +6,16 @@ int main()
     int i;
 
     for (i = 1; i < 100; i++) {
-        if (i % 15 == 0) {
-            printf("FizzBuzz");
-        }
-        else if (i % 21 == 0){
-            printf("FizzBouze");
-        }
-        else if (i % 35 == 0){
-            printf("BuzzBouze");
-        }
-        else if (i % 3 == 0) {
+        if (i % 3 == 0) {
             printf("Fizz");
         }
-        else if (i % 5 == 0) {
+        if (i % 5 == 0) {
             printf("Buzz");
         }
-        else if (i % 7 == 0) {
+        if (i % 7 == 0) {
             printf("Bouze");
         }
-        else {
+        if ((i % 3 != 0) && (i % 5 != 0) && (i % 7 != 0)) {
             printf("%d", i);
         }
 	    printf("\n");
